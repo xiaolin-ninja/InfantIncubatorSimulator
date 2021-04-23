431b4f561950f8aaca6bd78f34bcad259037ef01
Lok <LokYan@gmail.com>
2021-04-23 00:13:10 -0400

    Initial Commit

---
+++ SampleClient.py
@@ -0,0 +1,79 @@
+import threading
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import infinc
+import time
+import math
+
+class SimpleClient :
+    def __init__(self, therm1, therm2) :
+        self.fig, self.ax = plt.subplots()
+        now = time.time()
+        self.lastTime = now
+        self.times = [time.strftime("%H:%M:%S", time.localtime(now-i)) for i in range(30, 0, -1)]
+        self.infTemps = [0]*30
+        self.incTemps = [0]*30
+        self.infLn, = plt.plot(range(30), self.infTemps, label="Infant Temperature")
+        self.incLn, = plt.plot(range(30), self.incTemps, label="Incubator Temperature")
+        plt.xticks(range(30), self.times, rotation=45)
+        plt.ylim((20,50))
+        plt.legend(handles=[self.infLn, self.incLn])
+        self.infTherm = therm1
+        self.incTherm = therm2
+
+        self.ani = animation.FuncAnimation(self.fig, self.updateInfTemp, interval=500)
+        self.ani2 = animation.FuncAnimation(self.fig, self.updateIncTemp, interval=500)
+
+    def updateTime(self) :
+        now = time.time()
+        if math.floor(now) > math.floor(self.lastTime) :
+            t = time.strftime("%H:%M:%S", time.localtime(now))
+            self.times.append(t)
+            #last 30 seconds of of data
+            self.times = self.times[-30:]
+            self.lastTime = now
+            plt.xticks(range(30), self.times,rotation = 45)
+            plt.title(time.strftime("%A, %Y-%m-%d", time.localtime(now)))
+
+
+    def updateInfTemp(self, frame) :
+        self.updateTime()
+        self.infTemps.append(self.infTherm.getTemperature()-273)
+        #self.infTemps.append(self.infTemps[-1] + 1)
+        self.infTemps = self.infTemps[-30:]
+        self.infLn.set_data(range(30), self.infTemps)
+        return self.infLn,
+
+    def updateIncTemp(self, frame) :
+        self.updateTime()
+        self.incTemps.append(self.incTherm.getTemperature()-275)
+        #self.incTemps.append(self.incTemps[-1] + 1)
+        self.incTemps = self.incTemps[-30:]
+        self.incLn.set_data(range(30), self.incTemps)
+        return self.incLn,
+
+UPDATE_PERIOD = .05 #in seconds
+SIMULATION_STEP = .1 #in seconds
+
+#create a new instance of IncubatorSimulator
+bob = infinc.Human(mass = 8, length = 1.68, temperature = 36 + 273)
+bobThermo = infinc.SmartThermometer(bob, UPDATE_PERIOD)
+bobThermo.start() #start the thread
+
+inc = infinc.Incubator(width = 1, depth=1, height = 1, temperature = 37 + 273, roomTemperature = 20 + 273)
+incThermo = infinc.SmartThermometer(inc, UPDATE_PERIOD)
+incThermo.start() #start the thread
+
+incHeater = infinc.SmartHeater(powerOutput = 1500, setTemperature = 50 + 273, thermometer = incThermo, updatePeriod = UPDATE_PERIOD)
+inc.setHeater(incHeater)
+incHeater.start() #start the thread
+
+sim = infinc.Simulator(infant = bob, incubator = inc, roomTemp = 20 + 273, timeStep = SIMULATION_STEP, sleepTime = SIMULATION_STEP / 10)
+
+sim.start()
+
+sc = SimpleClient(bobThermo, incThermo)
+
+plt.grid()
+plt.show()
+
